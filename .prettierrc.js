1c8a2001db3fe3417476f6b5cbfc6e0577914a16
younes.mousavi <younesmousavi72@gmail.com>
2024-09-28 14:10:44 +0330

    feat(structural): add prettier to project

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  singleQuote: false,
+  htmlWhitespaceSensitivity: "strict",
+  vueIndentScriptAndStyle: true,
+  printWidth: 110,
+  arrowParens: "always",
+};
