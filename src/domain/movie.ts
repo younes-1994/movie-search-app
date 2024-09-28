a20d8c78da7b8ad4fe6ff7bbbbb6284dfc3000a8
younes.mousavi <younesmousavi72@gmail.com>
2024-09-28 14:13:03 +0330

    feat(domain): add MovieDetails type

---
+++ src/domain/movie.ts
@@ -0,0 +1,31 @@
+type Ratings = {
+  Source: string;
+  Value: string;
+};
+
+export type MovieDetails = {
+  Title: string;
+  Year: string;
+  Actors: string;
+  Awards: string;
+  BoxOffice: string;
+  Country: string;
+  DVD: string;
+  Director: string;
+  Genre: string;
+  Language: string;
+  Metascore: string;
+  Plot: string;
+  Poster: string;
+  Production: string;
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Type: string;
+  Website: string;
+  Writer: string;
+  imdbID: string;
+  imdbRating: string;
+  imdbVotes: string;
+  Ratings: Ratings[];
+};
