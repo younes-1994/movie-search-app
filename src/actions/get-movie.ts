fb826cf7409e4fe325193b1715d098b330c5a3d2
younes.mousavi <younesmousavi72@gmail.com>
2024-09-29 22:13:36 +0330

    fix(actions): replace and edit get-movie action

---
+++ src/actions/get-movie.ts
@@ -0,0 +1,27 @@
+//TODO: ERROR MIDDLEWARE AND FACTORY METHOD
+"use server";
+import { MovieParams } from "@/domain/movie";
+
+export async function getMovie({ title, type, page }: MovieParams) {
+  try {
+    let url = `https://www.omdbapi.com/?apikey=${process.env.NEXT_PUBLIC_OMDB_API_KEY}&s=${title}&page=${page}`;
+    let config = { next: { tags: ["getMovie", title, page.toString()], revalidate: 1 * 60 * 60 } };
+
+    if (type) {
+      url += `&page=${type}`;
+      config.next.tags.push(type);
+    }
+
+    const response = await fetch(url, config);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    if (data.Response === "False") {
+      throw new Error(data.Error);
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
